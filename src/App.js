2ea86ab79ad64ceb71bd36652ad7337919425cab
Feras Tajuore <ferastajuore@yahoo.com>
2019-09-23 16:06:31 +0200

    Design dashboard and set color for page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Header, Sidebar, Footer } from './layouts';
 import './styles/main.scss';
 
 const App = () => (
-    <div className="App">
-        React Project
+    <div className="app">
+        <div className="header">
+            <Header/>
+        </div>
+        <div className="content">
+            <Sidebar className="sidebar" />
+        </div>
     </div>
 );
 
